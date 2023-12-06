8c55b7993b6217a5a1d3829be204fd325604f9fe
Jeff <1203701249@qq.com>
2023-12-06 14:52:50 +0800

    jtag

--- jtag/tap_top.v
+++ jtag/tap_top.v
@@ -1,4 +1,12 @@
-//ref link: https://github.com/freecores/jtag/blob/master/tap/rtl/verilog/tap_top.v
+//ref link:[1] https://github.com/freecores/jtag/blob/master/tap/rtl/verilog/tap_top.v
+//          [2]https://www.fpga4fun.com/JTAG2.html 
+//          [3]https://www.xjtag.com/about-jtag/jtag-a-technical-overview/ 
 //final from: www.opencores.com
 //Jtag: TDI,TDO,TMS and TCK;
 //What is important is that TAP controller state machine
+//Shift-DR and shift-IR are used in combination with the TDI and TDO lines;
+
+//IR--Instruction Register;  DR:Data Register;
+//=================================================
+
+//DR--Data Register;each IR value selects a different DR;
