62640af89fc15be640abb29576545e6bf5f5ac37
Jeff <1203701249@qq.com>
2023-12-06 12:06:44 +0800

    add tap_top.v

---
+++ jtag/tap_top.v
@@ -0,0 +1,4 @@
+//ref link: https://github.com/freecores/jtag/blob/master/tap/rtl/verilog/tap_top.v
+//final from: www.opencores.com
+//Jtag: TDI,TDO,TMS and TCK;
+//What is important is that TAP controller state machine
